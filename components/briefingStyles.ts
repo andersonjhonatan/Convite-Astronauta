c56d041aa64984b60727e14b3ba4b6c4c8d26e33
Anderson Jhonatan <105141966+andersonjhonatan@users.noreply.github.com>
2026-08-13 20:21:40 -0300

    Combina estilos da abertura

---
+++ components/briefingStyles.ts
@@ -0,0 +1,5 @@
+import { StyleSheet } from 'react-native';
+import { briefingStylesA } from './briefingStylesA';
+import { briefingStylesB } from './briefingStylesB';
+
+export const briefingStyles = StyleSheet.create({ ...briefingStylesA, ...briefingStylesB });
